3326065bd4ed201795d3f24cc92a86ca999e4686
essserrr <blizzz93@gmail.com>
2022-01-15 16:50:43 +0900

    prevent actions test added

---
+++ packages/crt-terminal/src/API/keyboard/prevent-actions/prevent-actions.test.ts
@@ -0,0 +1,18 @@
+import 'jest';
+import { mock } from 'jest-mock-extended';
+import { preventActions, PreventProps } from './prevent-actions';
+
+jest.useFakeTimers();
+
+describe('Prevent default actions', () => {
+  describe('preventActions', () => {
+    it('should prevent default action', async () => {
+      const event = mock<PreventProps>();
+
+      preventActions(event);
+
+      expect(event.preventDefault).toBeCalled();
+      expect(event.preventDefault).toHaveBeenCalledTimes(1);
+    });
+  });
+});
